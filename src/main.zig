bc59799d0614962226951a450a852fad23a8296b
supxinfy <supxinfy@gmail.com>
2025-01-03 01:47:52 +0100

    Matrix computation

--- src/main.zig
+++ src/main.zig
@@ -30,4 +30,63 @@ fn prime_list(comptime n: u32, comptime len: u32) [len]u32 {
     return result;
 }
 
-pub fn main() !void {}
+const number_of_matrices = 8;
+const moduli = 1;
+const moduli_list = [1]u32{5};
+
+fn generate_matrices() [number_of_matrices][500][500][moduli]i32 {
+    var local_matrices: [number_of_matrices][500][500][moduli]i32 = undefined;
+    for (0..number_of_matrices) |s| {
+        for (0..s + 1) |i| {
+            for (0..s + 1) |j| {
+                for (0..moduli) |k| {
+                    local_matrices[s][i][j][k] = 0;
+                    if (s == 0) {
+                        local_matrices[s][0][0][k] = 1;
+                    } else {
+                        if (i < s and j < s) {
+                            local_matrices[s][i][j][k] = local_matrices[s - 1][i][j][k];
+                            if (i > 0 and j < s) {
+                                local_matrices[s][i][j][k] += local_matrices[s - 1][i - 1][j][k];
+                            }
+                            if (j > 0 and i < s) {
+                                local_matrices[s][i][j][k] += local_matrices[s - 1][i][j - 1][k];
+                            }
+                            if (i > 0 and j > 0) {
+                                local_matrices[s][i][j][k] -= local_matrices[s - 1][i - 1][j - 1][k];
+                            }
+                            if (i >= 1 and i <= s - 2 and s > 2) {
+                                local_matrices[s][i][j][k] /= 2;
+                            }
+                            //local_matrices[s][i][j][k] = @mod(local_matrices[s][i][j][k], moduli_list[k]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return local_matrices;
+}
+
+fn print_matrix(matrix: [500][500][moduli]i32, comptime idx: usize) void {
+    for (0..moduli) |k| {
+        std.debug.print("Matrix {}, modulo {}\n", .{ idx, moduli_list[k] });
+        for (0..idx) |i| {
+            for (0..idx) |j| {
+                std.debug.print("{} ", .{matrix[i][j][k]});
+            }
+            std.debug.print("\n", .{});
+        }
+    }
+}
+
+const matrices = generate_matrices();
+pub fn main() !void {
+    print_matrix(matrices[1], 1);
+    print_matrix(matrices[2], 2);
+    print_matrix(matrices[3], 3);
+    print_matrix(matrices[4], 4);
+    print_matrix(matrices[5], 5);
+    print_matrix(matrices[6], 6);
+    print_matrix(matrices[7], 7);
+}
